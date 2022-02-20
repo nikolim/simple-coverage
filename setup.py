6ab628b30e3d74f167822e83cf34d52f41fde169
Nikolai Limbrunner <ntli@kth.se>
2022-02-20 18:34:02 +0100

    Fix: increase version number for PyPI (#7)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="simple_coverage",
-    version="0.0.1",
+    version="0.1.0",
     author="Nikolai Limbrunner",
     author_email="nikolai.limbrunner@gmail.com",
     description="Simple Coverage measurements for Python",
