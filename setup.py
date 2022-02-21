7bbd55cf1c2f348c1d4b528d72d5d51027b0f8d6
Nikolai Limbrunner <ntli@kth.se>
2022-02-21 13:12:40 +0100

    Fix: setup.py package dir (#17)

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="simple_coverage",
-    version="0.1.2",
+    version="0.1.3",
     author="Nikolai Limbrunner",
     author_email="nikolai.limbrunner@gmail.com",
     description="Simple Coverage measurements for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nikolim/simple-coverage",
-    package_dir={"": "simple_coverage"},
-    packages=setuptools.find_packages(where="simple_coverage"),
+    package_dir={"simple_coverage": "simple_coverage"},
+    packages=["simple_coverage"],
     python_requires=">=3.6",
     install_requires=['termcolor'],
 )
